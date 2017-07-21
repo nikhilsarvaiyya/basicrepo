4607df99a4bbbe1d940a58a4e8d6cc7f7f9ff3d8
NIkhil <nsarvaiyya@presidio.com>
2017-07-21 14:37:43 +0530

    basic repo

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.children}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
